4322cc8e590f5b146a83bd68915ec9d15700511c
slawkens1 <slawkens@gmail.com>
2018-03-18 23:20:23 +0100

    * fix where container or item doesn't exist
    Doesn't show many Lua errors in console, but just simple warning.
    Additionally fixed displaying items count in queue (with Waiting
    status).
    Also corrected some messages.

--- gesior-shop-system.lua
+++ gesior-shop-system.lua
@@ -64,16 +64,17 @@ function getResults()
 		local results = {}
 		repeat
 			local tmp = {}
-			tmp.id = result.getDataInt(resultId, "id")
 			tmp.name = result.getDataString(resultId, "name")
-			tmp.action = result.getDataString(resultId, "action")
-			tmp.delete_it = result.getDataInt(resultId, "delete_it")
-
+			
 			-- better performance when no player found
 			tmp.exist = false
 			tmp.cid = getPlayerByName(tmp.name)
 			if(tmp.cid and isPlayer(tmp.cid)) then
 				tmp.exist = true
+				
+				tmp.id = result.getDataInt(resultId, "id")
+				tmp.action = result.getDataString(resultId, "action")
+				tmp.delete_it = result.getDataInt(resultId, "delete_it")
 				
 				tmp.param1 = result.getDataInt(resultId, "param1")
 				tmp.param2 = result.getDataInt(resultId, "param2")
@@ -81,8 +82,9 @@ function getResults()
 				tmp.param4 = result.getDataInt(resultId, "param4")
 				tmp.param5 = result.getDataString(resultId, "param5")
 				tmp.param6 = result.getDataString(resultId, "param6")
-				table.insert(results, tmp)
 			end
+			
+			table.insert(results, tmp)
 		until not result.next(resultId)
 		result.free(resultId)
 		
@@ -97,10 +99,7 @@ function getResults()
 			local results = {}
 			repeat
 				local tmp = {}
-				tmp.id = tonumber(result_plr:getDataInt("id"))
 				tmp.name = tostring(result_plr:getDataString("name"))
-				tmp.action = tostring(result_plr:getDataString("action"))
-				tmp.delete_it = tonumber(result_plr:getDataInt("delete_it"))
 
 				-- better performance when no player found
 				tmp.exist = false
@@ -108,14 +107,19 @@ function getResults()
 				if(tmp.cid and isPlayer(tmp.cid)) then
 					tmp.exist = true
 
+					tmp.id = tonumber(result_plr:getDataInt("id"))
+					tmp.action = tostring(result_plr:getDataString("action"))
+					tmp.delete_it = tonumber(result_plr:getDataInt("delete_it"))
+					
 					tmp.param1 = tonumber(result_plr:getDataInt("param1"))
 					tmp.param2 = tonumber(result_plr:getDataInt("param2"))
 					tmp.param3 = tonumber(result_plr:getDataInt("param3"))
 					tmp.param4 = tonumber(result_plr:getDataInt("param4"))
 					tmp.param5 = tostring(result_plr:getDataString("param5"))
 					tmp.param6 = tostring(result_plr:getDataString("param6"))
-					table.insert(results, tmp)
 				end
+				
+				table.insert(results, tmp)
 			until not(result_plr:next())
 
 			result_plr:free()
@@ -153,7 +157,7 @@ function onThink(interval)
 
 	local started = os.mtime and os.mtime() or os.time()
 	local addedItems, waitingItems = 0, 0
-	local added
+	local added = false
 
 	local results = getResults()
 	if(not results) then
@@ -168,55 +172,66 @@ function onThink(interval)
 
 		if(v.exist) then
 			local cid = v.cid
-			local param1 = v.param1
-			local param2 = v.param2
-			local param3 = v.param3
-			local param4 = v.param4
+			local param1, param2, param3, param4 = v.param1, v.param2, v.param3, v.param4
 			local add_item_type = v.param5
 			local add_item_name = v.param6
-			local received_item = 0
-			local full_weight = 0
-			if(add_item_type == 'container') then
-				local items_weight = 0
-				container_weight = getItemWeightById(param3, 1)
-				if(isItemRune(param1)) then
-					items_weight = param4 * getItemWeightById(param1, 1)
-				else
-					items_weight = param4 * getItemWeightById(param1, param2)
-				end
-				full_weight = items_weight + container_weight
-			elseif(add_item_type == 'item') then
-				full_weight = getItemWeightById(param1, param2)
-				if(isItemRune(param1)) then
-					full_weight = getItemWeightById(param1, 1)
-				end
-			end
+			local received_item, full_weight, items_weight, item_weigth = 0, 0, 0, 0
+			local item_doesnt_exist = false
 
 			if(add_item_type == 'container' or add_item_type == 'item') then
-				local free_cap = getPlayerFreeCap(cid)
-				if(full_weight <= free_cap) then
-					if(add_item_type == 'container') then
-						local new_container = doCreateItemEx(param3, 1)
-						for x = 1, param4 do
-							doAddContainerItem(new_container, param1, param2)
-						end
-						received_item = doPlayerAddItemEx(cid, new_container)
-					else
-						local new_item = doCreateItemEx(param1, param2)
-						received_item = doPlayerAddItemEx(cid, new_item)
-					end
-
-					if(received_item == RETURNVALUE_NOERROR) then
-						doPlayerSendTextMessage(cid, messageType, "You received >> ".. add_item_name .." << from OTS shop.")
-						doQuery("DELETE FROM `z_ots_comunication` WHERE `id` = " .. id .. ";")
-						doQuery("UPDATE `z_shop_history` SET `trans_state`='realized', `trans_real`=" .. os.time() .. " WHERE comunication_id = " .. id .. ";")
-						doPlayerSave(cid)
-						added = true
-					else
-						doPlayerSendTextMessage(cid, messageType, '>> '.. add_item_name ..' << from OTS shop is waiting for you. Please make place for this item in your backpack/hands and wait about '.. interval ..' seconds to get it.')
-					end
+				local item_weigth = getItemWeightById(param1, 1)
+				if(type(item_weigth) == 'boolean') then -- item doesn't exist
+					print("[ERROR - gesior-shop-system] Invalid item id: " .. param1 .. ". Change/Fix `itemid1` in `z_shop_offers` then delete it from `z_ots_comunication`")
+					item_doesnt_exist = true
 				else
-					doPlayerSendTextMessage(cid, messageType, '>> '.. add_item_name ..' << from OTS shop is waiting for you. It weight is '.. full_weight ..' oz., you have only '.. free_cap ..' oz. free capacity. Put some items in depot and wait about '.. interval ..' seconds to get it.')
+					if(add_item_type == 'container') then
+						container_weight = getItemWeightById(param3, 1)
+						if(type(container_weight) == 'boolean') then -- container item doesn't exist
+							print("[ERROR - gesior-shop-system] Invalid container id: " .. param3 .. ". Change/Fix `itemid2` in `z_shop_offers` then delete it from `z_ots_comunication`")
+							item_doesnt_exist = true
+						else
+							if(isItemRune(param1)) then
+								items_weight = param4 * item_weigth
+							else
+								items_weight = param4 * getItemWeightById(param1, param2)
+							end
+							
+							full_weight = items_weight + container_weight
+						end
+					elseif(add_item_type == 'item') then
+						full_weight = getItemWeightById(param1, param2)
+						if(isItemRune(param1)) then
+							full_weight = getItemWeightById(param1, 1)
+						end
+					end
+				end
+				
+				if(not item_doesnt_exist) then
+					local free_cap = getPlayerFreeCap(cid)
+					if(full_weight <= free_cap) then
+						if(add_item_type == 'container') then
+							local new_container = doCreateItemEx(param3, 1)
+							for x = 1, param4 do
+								doAddContainerItem(new_container, param1, param2)
+							end
+							received_item = doPlayerAddItemEx(cid, new_container)
+						else
+							local new_item = doCreateItemEx(param1, param2)
+							received_item = doPlayerAddItemEx(cid, new_item)
+						end
+
+						if(received_item == RETURNVALUE_NOERROR) then
+							doPlayerSendTextMessage(cid, messageType, "You received >> ".. add_item_name .." << from OTS shop.")
+							doQuery("DELETE FROM `z_ots_comunication` WHERE `id` = " .. id .. ";")
+							doQuery("UPDATE `z_shop_history` SET `trans_state`='realized', `trans_real`=" .. os.time() .. " WHERE comunication_id = " .. id .. ";")
+							doPlayerSave(cid)
+							added = true
+						else
+							doPlayerSendTextMessage(cid, messageType, '>> '.. add_item_name ..' << from OTS shop is waiting for you. Please make place for this item in your backpack/hands and wait about '.. interval ..' seconds to get it.')
+						end
+					else
+						doPlayerSendTextMessage(cid, messageType, '>> '.. add_item_name ..' << from OTS shop is waiting for you. It weight is '.. full_weight ..' oz., you have only '.. free_cap ..' oz. free capacity. Put some items in depot and wait about '.. interval ..' seconds to get it.')
+					end
 				end
 			elseif(add_item_type == 'addon') then
 				doPlayerSendTextMessage(cid, messageType, "You received >> ".. add_item_name .." << from OTS shop.")
@@ -229,7 +244,7 @@ function onThink(interval)
 				added = true
 			elseif(add_item_type == 'mount') then
 				if(not doPlayerAddMount) then
-					print("[ERROR - gesior-shop-system] Your server doesn't support mounts. Remove all items from your `z_shop_offers` database table where `offer_type` = mount.")
+					print("[ERROR - gesior-shop-system] Your server doesn't support mounts. Remove all items in database from your `z_shop_offers` table where `offer_type` = mount and also in `z_ots_comunication` where `param5` = mount.")
 				else
 					doPlayerAddMount(cid, param1)
 					doPlayerSendTextMessage(cid, messageType, "You received >> ".. add_item_name .." << from OTS shop.")
